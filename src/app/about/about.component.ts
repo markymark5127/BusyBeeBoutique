d2154e8e0001060c997e9bcac351f6ddd984a71c
markymark5127 <markmayne@ymail.com>
2021-03-14 00:25:54 -0500

    changin about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  public innerWidth: any;
+  public innerHeight: any;
+  public isMobile = false;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    this.innerHeight = window.innerHeight;
+    if (this.innerHeight > this.innerWidth) {
+      this.isMobile = true;
+    }
   }
 
 }
