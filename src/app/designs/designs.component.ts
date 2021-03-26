bcf7f431e882d929889315ee2a5e15a857d3f057
markymark5127 <markmayne@ymail.com>
2021-03-26 16:12:42 -0400

    changed the home screen a little bit and added in a few cups

--- src/app/designs/designs.component.ts
+++ src/app/designs/designs.component.ts
@@ -39,19 +39,54 @@ export class DesignsComponent implements OnInit {
     { size: "15 oz. Skinny", price: 27},
     { size: "20 oz. Skinny", price: 32},
     { size: "20 oz. Modern Curve", price: 32},
-    { size: "30 oz. Skinny", price: 42},
-    { size: "30 oz. Modern Curve", price: 42},
     { size: "20 oz. Strawbler", price: 35},
     { size: "20 oz. Skinny Strawbler", price: 35},
+    { size: "30 oz. Skinny", price: 42},
+    { size: "30 oz. Modern Curve", price: 42},
     { size: "30 oz. Skinny Strawbler", price: 45}
+  ]
+  uf: Pricing[] = [
+    { size: "8 oz. Sippy", price: 27},
+    { size: "12 oz. Transitional Sippy", price: 27},
+    { size: "12 oz. Curvy", price: 27},
+    { size: "14 oz. Coffee Mug", price: 27},
+    { size: "15 oz. Skinny", price: 35},
+    { size: "20 oz. Skinny", price: 40},
+    { size: "20 oz. Modern Curve", price: 40},
+    { size: "20 oz. Skinny + Screw Lid", price: 42},
+    { size: "20 oz. Strawbler", price: 42},
+    { size: "20 oz. Skinny Strawbler", price: 42},
+    { size: "30 oz. Skinny", price: 50},
+    { size: "30 oz. Modern Curve", price: 50},
+    { size: "30 oz. Modern Strawbler", price: 52}
+  ]
+  cn: Pricing[] = [
+    { size: "15 oz. Skinny", price: 35},
+    { size: "20 oz. Skinny", price: 40},
+    { size: "20 oz. Modern Curve", price: 40},
+    { size: "20 oz. Skinny + Screw Lid", price: 42},
+    { size: "20 oz. Strawbler", price: 42},
+    { size: "20 oz. Skinny Strawbler", price: 42},
+    { size: "30 oz. Skinny", price: 50},
+    { size: "30 oz. Modern Curve", price: 50},
+    { size: "30 oz. Modern Strawbler", price: 52}
+
   ]
 
   cups: Product[] = [
-
+    // Add a new line and fill this template out in a new line
+    // price: put the price of the smallest/first size cup
+    // size: put the first size
+    // img: put "/assets/[imagename.extension]"
+    // priceopt: put one of the values created above
+    //{ name: "", price: , desc: "", size: "", img:"/assets/", priceOpt: },
     { name: "Tri-Color Hydro Dip", price: 30, desc: "Personalized cup with a exciting and unique patterned design", size: "15 oz. Skinny", img:"/assets/TricolorHydroDipMadison.jpeg", priceOpt: this.tchd  },
     { name: "Colorful Cloud 9", price: 30, desc: "A specialized design that gives a cloud effect with a hint of sparkle", size: "15 oz. Skinny", img:"/assets/GlitterCloud.jpeg", priceOpt: this.cc9},
     { name: "Chetah Wine", price: 20, desc: "A wine glass with optional fun sayings such as \"Hammered\"", size: "Lit", img:"/assets/CheetahPrintWine.jpeg", priceOpt: this.cw},
-    { name: "Holy Cow!", price: 27, desc: "A flower crowned cow with the slogan \"Not Today Heifer\"", size: "15 oz. Skinny", img:"/assets/HolyCow.jpeg", priceOpt: this.hc }
+    { name: "Holy Cow!", price: 27, desc: "A flower crowned cow with the slogan \"Not Today Heifer\"", size: "15 oz. Skinny", img:"/assets/HolyCow.jpeg", priceOpt: this.hc },
+    { name: "Unicorn Fart", price: 27, desc: "A flower crowned cow with the slogan \"Not Today Heifer\"", size: "15 oz. Skinny", img:"/assets/GlitterName.JPG", priceOpt: this.uf },
+    { name: "Cheetah Mama", price: 27, desc: "A flower crowned cow with the slogan \"Not Today Heifer\"", size: "15 oz. Skinny", img:"/assets/CheetahName.JPG", priceOpt: this.cn }
+
     //{ name: "The Lepriclaw", price: 40, desc: "A tumbler with a St.Patricks Day themed parody of the iconic whiteclaw design", size: 20, img:"/assets/lepriclaw.jpg" }
   ];
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,21 +17,17 @@ export interface Tile {
 })
 export class HomeComponent implements OnInit {
   tiles: Tile[] = [
+    {text: '/assets/reesesCocoaBomb.jpeg', cols: 1, rows: 1, color: 'lightblue'},
     {text: '/assets/twea.jpeg', cols: 1, rows: 2, color: 'lightblue'},
     {text: '/assets/farmFresh.jpeg', cols: 1, rows: 1, color: 'lightblue'},
     {text: '/assets/lucielle.jpeg', cols: 1, rows: 2, color: 'lightblue'},
-    {text: '/assets/PartyInTheBackMat.jpeg', cols: 1, rows: 2, color: 'lightblue'},
-    {text: '/assets/Earrings1.jpeg', cols: 1, rows: 1, color: 'lightblue'},
-    {text: '/assets/SongBox.jpeg', cols: 1, rows: 1, color: 'lightblue'},
-    {text: '/assets/busybee_trans.png', cols: 2, rows: 2, color: 'lightblue'},
-    {text: '/assets/luckyCharmCocoa.jpeg', cols: 1, rows: 1, color: 'lightblue'},
-    {text: '/assets/theChild.jpeg', cols: 1, rows: 2, color: 'lightblue'},
-    {text: '/assets/mamaBearEarings.jpeg', cols: 1, rows: 2, color: 'lightblue'},
-    {text: '/assets/reesesCocoaBomb.jpeg', cols: 1, rows: 1, color: 'lightblue'},
-    {text: '/assets/rugratz.jpeg', cols: 1, rows: 2, color: 'lightblue'},
     {text: '/assets/sisFlowers.jpeg', cols: 1, rows: 2, color: 'lightblue'},
-    {text: '/assets/sugarCookie.jpeg', cols: 1, rows: 1, color: 'lightblue'},
-    {text: '/assets/weThePeople.jpeg', cols: 1, rows: 2, color: 'lightblue'},
+    {text: '/assets/busybee_trans.png', cols: 2, rows: 2, color: 'lightblue'},
+    {text: '/assets/mamaBearEarings.jpeg', cols: 1, rows: 2, color: 'lightblue'},
+    {text: '/assets/PartyInTheBackMat.jpeg', cols: 1, rows: 2, color: 'lightblue'},
+    {text: '/assets/rugratz.jpeg', cols: 1, rows: 2, color: 'lightblue'},
+    {text: '/assets/Earrings1.jpeg', cols: 1, rows: 1, color: 'lightblue'},
+    {text: '/assets/luckyCharmCocoa.jpeg', cols: 1, rows: 1, color: 'lightblue'}
   ];
 
   public Imagedata = [
